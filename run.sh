39623bf129ac7275e58cbf822f7f84d223b7d073
yangyang <mtbyang@gmail.com>
2018-07-27 17:27:59 +0800

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+
+# tmux
+echo "tmux part"
+if [ ! -d ~/.tmux/plugins/tpm ]
+then
+    echo "clone tpm"
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 ln -i tmux/tmux.conf ~/.tmux.conf
+echo "prefix + I to install plugin"
+
+# zsh
+echo "zsh part"
+if [ ! -d ~/.oh-my-zsh ]
+then
+    echo "clone zsh"
+    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
 ln -i zsh/zshrc ~/.zshrc
-ln -i emacs/spacemacs ~/.spacemacs
+echo "chsh -s /bin/zsh to zsh"
+#ln -i emacs/spacemacs ~/.spacemacs
